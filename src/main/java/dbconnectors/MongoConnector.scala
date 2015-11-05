33b88e80cba0afac361da5c918e2749624c8942c
= <=>
2015-11-05 12:38:43 +0000

    mongo messing

--- src/main/java/dbconnectors/MongoConnector.scala
+++ src/main/java/dbconnectors/MongoConnector.scala
@@ -3,7 +3,7 @@ package dbconnectors
 import org.mongodb.scala.MongoClient
 import org.mongodb.scala.MongoDatabase
 import org.mongodb.scala.MongoCollection
-import org.mongodb.scala.Document
+import org.mongodb.scala.bson.collection.mutable.Document
 
 /**
  * @author tstacey
@@ -13,4 +13,21 @@ class MongoConnector {
   val database: MongoDatabase = mongoClient.getDatabase("warehouseTracking")
   
   val collection:MongoCollection[Document] = database.getCollection("addresses")
+  
+  def tst():Unit = {
+      collection.find().first()
+  }
+  
+  def close() = {
+    mongoClient.close()
+  }
+}
+
+object MongoTest {
+  
+  def main(args: Array[String]): Unit = {
+    val mTest = new MongoConnector()
+    mTest.tst()
+    mTest.close()
+  }
 }
