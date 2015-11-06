019fbaae85291b00112d97650e286c469ff8a46e
= <=>
2015-11-06 09:31:09 +0000

    Items now reading from Mongo

--- src/main/java/dbconnectors/MongoConnector.scala
+++ src/main/java/dbconnectors/MongoConnector.scala
@@ -2,6 +2,7 @@ package dbconnectors
 
 import com.mongodb.casbah.Imports._
 import entities.Address
+import entities.Item
 
 /**
  * @author tstacey
@@ -11,6 +12,11 @@ object MongoConnector {
   
   val database = mongoClient("warehouseTracking")
   val addressCollection = database("addresses")
+  val itemCollection = database("items")
+  
+  def close() = {
+    mongoClient.close()
+  }
   
   /**
    * returns an address entity from the MongoDB corresponding to the passed address ID
@@ -21,25 +27,41 @@ object MongoConnector {
     makeAddressEntityFromMongoDBObject(addrDoc.get)
   }
   
-  def close() = {
-    mongoClient.close()
-  }
-  
   /**
    * returns an Address Entity from a mongo object
    */
   private def makeAddressEntityFromMongoDBObject(addrObj:MongoDBObject):Address = {
     val addrLines:Map[String,String] = addrObj.getAs[Map[String,String]]("AddressLines").get
-    Address(addrObj.getAs[Double]("AddressID").get.toInt, addrLines, addrObj.getAs[String]("City").get, addrObj.getAs[String]("County").get, addrObj.getAs[String]("PostCode").get)
+    Address(addrObj.getAs[Double]("AddressID").get.toInt,
+        addrLines, addrObj.getAs[String]("City").get,
+        addrObj.getAs[String]("County").get,
+        addrObj.getAs[String]("PostCode").get)
+  }
+  
+  
+  def getItem(itemID:Int):Item = {
+    val queryObject = MongoDBObject("ItemID" -> itemID)
+    val itemDoc = itemCollection.findOne(queryObject)
+    makeItemEntityFromMongoDBObject(itemDoc.get)
+  }
+  
+  private def makeItemEntityFromMongoDBObject(itemObj:MongoDBObject):Item = {
+    val itemAttributes:Map[String,String] = itemObj.getAs[Map[String,String]]("ItemAttributes").get
+    Item(itemObj.getAs[Double]("ItemID").get.toInt,
+        itemObj.getAs[String]("ItemName").get,
+        itemObj.getAs[String]("ItemDescription").get,
+        itemObj.getAs[Double]("ItemVolume").get.toInt,
+        itemObj.getAs[String]("ImageLocation").get,
+        itemObj.getAs[Double]("SupplierID").get.toInt,
+        itemAttributes)
   }
 }
 
 object MongoTest {
   
   def main(args: Array[String]): Unit = {
-    val i:Int = 1;
-    val addr:Address = MongoConnector.getAddress(i)
-    addr.print()
+    val itm:Item = MongoConnector.getItem(1)
+    itm.print()
     
     MongoConnector.close()
   }
--- src/main/java/entities/Item.scala
+++ src/main/java/entities/Item.scala
@@ -3,11 +3,12 @@ package entities
 /**
  * @author tstacey
  */
-case class Item(itemID:Int, itemName:String, itemDescription:String, imageLocation:String, idSupplier:Int, itemAttributes:Map[String,String]) {
+case class Item(itemID:Int, itemName:String, itemDescription:String, itemVolume:Int, imageLocation:String, idSupplier:Int, itemAttributes:Map[String,String]) {
   def print() {
     println("Item ID: "+itemID)
     println("Name: "+itemName)
     println("Description: "+itemDescription)
+    println("Volume: "+itemVolume+"ml")
     println("Image Location: "+imageLocation)
     println("Supplier ID: "+idSupplier)
     println("Attributes")
